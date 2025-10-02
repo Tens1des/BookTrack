c4b4984c14dce355e74019a692caba07ba66407b
Tens1des <romankotov3213@gmail.com>
2025-10-02 19:35:14 +0300

    Initial Commit

---
+++ BookTrack/BookTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackApp.swift
+//  BookTrack
+//
+//  Created by Рома Котов on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
